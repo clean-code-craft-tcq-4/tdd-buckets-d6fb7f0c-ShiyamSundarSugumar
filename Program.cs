65de4d8de22757b4667073f12dc4294235406011
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-08-18 15:46:59 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,10 +14,11 @@ namespace ConsoleApp2
         {
             Segregator segregator = new Segregator();
             List<int> data = new List<int>() {3,4,5,6,8,7,8,3,13,14,15,16,17,18,13,22,23};
-
-            string dd = segregator.find_data(data);
-            Console.WriteLine(dd);
-            Debug.Assert(segregator.find_data(data) == "{3-8: 8} {13-18: 7} {22-23: 2} ");
+            Debug.Assert(segregator.find_data(data) == "{3-8: 8} {13-18: 7} {22-23: 2} ");            
+            
+            List<int> data = new List<int>() {100,110,120,101,111,121,102,112,122,150};
+            Debug.Assert(segregator.find_data(data) == "{100-102, 3} {110-112, 3} {120-122, 4} ");
+            
 
         }
     }
