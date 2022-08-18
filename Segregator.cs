b1f796cab4aa8e4fb431970d2aee11b2a9649167
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-08-18 14:38:55 +0530

    Update Segregator.cs

--- Segregator.cs
+++ Segregator.cs
@@ -46,37 +46,5 @@ namespace ConsoleApp2
             }
             return val;
         }
-
-        public void Seq1 (List<int> arr, ref object start, ref object end,ref int i, ref int k)
-        {
-            if ((arr[i] == arr[i + 1]) || ((arr[i] + 1) == arr[i + 1]))
-            {
-                Seq2(arr, ref start, ref end, ref i, ref k);
-            }
-            else
-            {
-                Seq3(arr, ref start, ref end, ref i, ref k);
-            }
-        }
-
-        public void Seq2 (List<int> arr, ref object start, ref object end, ref int i, ref int k)
-        {
-            k++;
-            if (start == null) start = arr[i];
-            if (i == arr.Count - 2)
-            {
-                end = arr[i + 1];
-                k++;
-            }
-        }
-
-        public void Seq3(List<int> arr, ref object start, ref object end, ref int i, ref int k)
-        {
-            if (start != null)
-            {
-                end = arr[i]; k++;
-            }
-        }
-
     }
 }
