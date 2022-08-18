68f02ea2fa6660c7b9c351007006831f5c70122a
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-08-18 15:50:54 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,8 +16,8 @@ namespace ConsoleApp2
             List<int> data = new List<int>() {3,4,5,6,8,7,8,3,13,14,15,16,17,18,13,22,23};
             Debug.Assert(segregator.find_data(data) == "{3-8: 8} {13-18: 7} {22-23: 2} ");            
             
-            data = {100,110,120,101,111,121,102,112,122,150};
-            Debug.Assert(segregator.find_data(data) == "{100-102, 3} {110-112, 3} {120-122, 4} ");
+            List<int> data1 = new List<int>() {100,110,120,101,111,121,102,112,122,150};
+            Debug.Assert(segregator.find_data(data1) == "{100-102, 3} {110-112, 3} {120-122, 4} ");
             
 
         }
