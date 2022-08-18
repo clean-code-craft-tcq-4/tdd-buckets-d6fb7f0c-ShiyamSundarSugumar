60418769221ebf92fca0bc75c80b518ce0f28d8a
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-08-18 15:38:37 +0530

    Update logic.cs

--- logic.cs
+++ logic.cs
@@ -26,11 +26,9 @@ namespace ConsoleApp2
             if (start == null) start = arr[i];
             if (i == arr.Count - 2)
             {
-                end = arr[i + 1];
-                k++;
+                end = arr[i + 1];k++;              
             }
         }
-
         static void Seq3(List<int> arr, ref object start, ref object end, ref int i, ref int k)
         {
             if (start != null)
