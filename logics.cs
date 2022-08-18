d320ef3a42c0e4fb5cd173c9d57bf4d89c96f4ed
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-08-18 15:21:48 +0530

    Update logics.cs

--- logics.cs
+++ logics.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    class logics
+    public class logics
     {
-        public static void Seq1(List<int> arr, ref object start, ref object end, ref int i, ref int k)
+        public void Seq1(List<int> arr, ref object start, ref object end, ref int i, ref int k)
         {
             if ((arr[i] == arr[i + 1]) || ((arr[i] + 1) == arr[i + 1]))
             {
